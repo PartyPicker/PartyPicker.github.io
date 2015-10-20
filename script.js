1431d2e5385a810e36ccdcea57938ced70c4b706
Chozabu <chozabu@gmail.com>
2015-10-20 17:05:17 +0100

    put chart in div

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ var yAxis = d3.svg.axis()
     .orient("left")
     .tickFormat(d3.format("1s"));
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#chart-box").append("svg")
 //var svg = d3.select("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
